4dd4e91c51c423c69f3eeba962f39762d79d1455
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-01-09 12:40:11 +1100

    Add simple test

--- cmd/asciitree/main.go
+++ cmd/asciitree/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"asciitree/internal/tree"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello world")
 
-	// serializedTree := "1,2,#,#,3,4,#,#,5,#,#"
-	// root := deserialize(serializedTree)
+	serializedTree := "1,uwu,#,#,3,4,#,#,5,#,#"
+	root := tree.Deserialize(serializedTree)
+	tree.PrintLevelOrder(root)
 }
